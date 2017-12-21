3078fa7ca94d245eb303ace4a9f057db5ea328ad
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2017-12-21 22:08:23 +0700

    somehow figured out the basics of how to pass states and props for now

---
+++ Model/MainStore.js
@@ -0,0 +1,19 @@
+import {
+    reaction,
+    observable,
+    computed,
+    autorun,
+    action,
+  } from 'mobx';
+
+class MainStore {
+  
+    @observable myName = 'keanu'
+  
+    updateName(newName){
+      this.myName = newName
+    }
+  
+}
+
+export default new  MainStore()
