d60ed49567881ea035d58d799ca45734c107b0b8
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2017-12-25 10:56:12 +0700

    begin number crunching

--- Model/MainStore.js
+++ Model/MainStore.js
@@ -9,11 +9,15 @@ import {
 class MainStore {
   
     @observable myName = 'keanu'
-    
+    @observable myNumber = 0
+
     updateName(newName){
       this.myName = newName
     }
-  
+    
+    sumNumber(operand){
+      this.myNumber += operand
+    }
 }
 
 export default new  MainStore()
