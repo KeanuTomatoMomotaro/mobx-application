71d742ded6b95d9047971239eebcd700797cde9e
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2017-12-26 11:42:45 +0700

    fix the logic to calculate the summmmm

--- App.js
+++ App.js
@@ -16,30 +16,53 @@ export default class App extends React.Component {
     console.log("name is now",store.myName)
   }
 
+  // handlePressNumber(){
+  //   const store = MainStore
+  //   store.updateNumber(2) 
+  // }
+
+  // handlePressOperand(){
+  //   const store = MainStore
+  //   store.updateOperand(2) 
+  // }
+
+  // handlePressSum(value){
+  //   const store = MainStore
+  //   store.sumNumber() 
+  // }
+
   handlePressNumber(){
     const store = MainStore
-    store.updateNumber(2) 
+    store.updateNumberProp(2) 
   }
 
   handlePressOperand(){
     const store = MainStore
-    store.updateOperand(2) 
+    store.updateOperandProp(2) 
   }
 
-  handlePressSum(value){
+  handlePressOperator(){
     const store = MainStore
-    store.sumNumber() 
+    store.updateOperatorProp('+')
+  }
+
+  handlePressSum(){
+    const store = MainStore
+    store.updateSumProp(store.calculatorValues.number, store.calculatorValues.operator, store.calculatorValues.operand)
   }
 
   render() {
     const store = MainStore
     return (
       <View style = {{flex:1, alignItems:'center', justifyContent:'center'}}>
-        <Text>Number is {store.myNumber}</Text>        
-        <Text>Operand is {store.myOperand}</Text>      
+        <Text>Number is {store.calculatorValues.number}</Text>        
+        <Text>Operator is {store.calculatorValues.operator}</Text>      
+        <Text>Operand is {store.calculatorValues.operand}</Text>
+        <Text>Sum is {store.calculatorValues.sum}</Text>              
         <TouchableOpacity style={{backgroundColor:'blue', height:100, width:100}} onPress={this.handlePressNumber}></TouchableOpacity>
+        <TouchableOpacity style={{backgroundColor:'yellow', height:100, width:100}} onPress={this.handlePressOperator}></TouchableOpacity>  
         <TouchableOpacity style={{backgroundColor:'red', height:100, width:100}} onPress={this.handlePressOperand}></TouchableOpacity>  
-        <TouchableOpacity style={{backgroundColor:'yellow', height:100, width:100}} onPress={this.handlePressSum}></TouchableOpacity>  
+        <TouchableOpacity style={{backgroundColor:'green', height:100, width:100}} onPress={this.handlePressSum}></TouchableOpacity>  
       </View>
     );
   }
--- Model/MainStore.js
+++ Model/MainStore.js
@@ -11,6 +11,14 @@ class MainStore {
     @observable myName = 'keanu'
     @observable myNumber = 0
     @observable myOperand = 0
+    
+    //diff style
+    @observable calculatorValues = {
+          'number' : 0,
+          'operand' : 0,
+          'operator':'',
+          'sum' : 0,
+    }
 
     updateName(newName){
       this.myName = newName
@@ -26,6 +34,33 @@ class MainStore {
 
     sumNumber(){
       this.myNumber += this.myOperand
+    }
+
+    //diff style
+    @action updateNumberProp(number) {
+      this.calculatorValues.number = number
+      console.log('@action | Update number | number : ', this.calculatorValues.number)
+    }
+    
+    @action updateOperandProp(operand) {
+      this.calculatorValues.operand = operand
+      console.log('@action | Update operand | operand : ', this.calculatorValues.operand)
+    }
+
+    @action updateOperatorProp(operator) {
+      this.calculatorValues.operator = operator
+      console.log('@action | Update operator | operator : ', this.calculatorValues.operator)
+    }
+
+    @action updateSumProp(number, operand, operator) {
+      switch(operand){
+        case '+':
+          this.calculatorValues.sum = number + operator
+          break;
+        default:
+          break;
+      }
+      console.log('@action | Update Sum | sum : ', this.calculatorValues.sum)
     }
 }
 
